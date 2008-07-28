09e40e4e89636df51b125509d57413c1f8f94f76
shaderlab <shaderlab@e77911d0-6a52-0410-bea1-fbff64990c85>
2008-07-28 20:36:12 +0000

    IE support without sandbox
    git-svn-id: https://nbio.googlecode.com/svn/jsonq/trunk@20 e77911d0-6a52-0410-bea1-fbff64990c85

--- jsonq.js
+++ jsonq.js
@@ -7,9 +7,9 @@ Licensed under Apache License, Version 2.0 - http://www.apache.org/licenses/LICE
 (function(){
     var j = window.JSONQ = {};
     j.counter = 0;
-    window.JSONQ.requests = {};
+    j.requests = {};
     
-    j.eh = function(s) {
+    j.escapeHTML = function(s) {
         return s.replace( /([<>&""''])/g,
             function(m, c) {
                 switch( c ) {
@@ -23,29 +23,37 @@ Licensed under Apache License, Version 2.0 - http://www.apache.org/licenses/LICE
             } );
     };
     
+    j.sandbox = function(w) {
+        try {
+            w.parent = null;
+        } catch(e) {}
+    };
+   
     j.get = function(url, callback) {
         var id = (++j.counter);
         var i = document.createElement( "iframe" );
         i.style.display = "none";
         document.documentElement.appendChild( i );
-        
-        url = j.eh(url);
-        
-        var d = i.contentDocument || i.contentWindow.dument;
+                
+        var d = i.contentDocument || i.contentWindow.document;
         d.open("text/html", false);
-        d.write("<html><head></head><body>");
+        d.write("<html><body>");
         d.write("<script type='text/javascript'>");
-        d.write("(function(){var w = window; var p = w.parent; w.alert = w.cookie = w.parent = w.top = undefined; window.callback = function(obj){p.JSONQ.callback(obj, '" + id + "');};})();");
+        d.write("(function(){var w = window; var p = w.parent; p.JSONQ.sandbox(w); w.callback = function(obj){p.JSONQ.callback(obj, '" + id + "');};})();");
         d.write("</script>");
-        d.write("<script type='text/javascript' src='" + url + "'></script>");
         d.write("</body></html>");
         d.close();
         
+        var b = d.getElementsByTagName("body")[0];
+        var s = d.createElement("script");
+        s.setAttribute("type", "text/javascript");
+        s.setAttribute("src", url);
+        b.appendChild(s);
+
         return j.requests[id] = { iframe: i, callback: callback };
     };
     
     j.callback = function(obj, id) {
-        //console.log("Request ID: " + id + " Response: " + obj);
         var r = window.JSONQ.requests[id];
         delete window.JSONQ.requests[id];
         r.callback(obj);
